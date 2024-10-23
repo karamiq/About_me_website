6b0a4f429ca350c858dcdd8644f3c4f7047a7dab
karamiq <fs58fryfbyqc@gmail.com>
2024-10-24 01:33:54 +0300

    Deploying to about_me_website

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"36335019a8eab588c3c2ea783c618d90505be2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1361553979"
+    serviceWorkerVersion: "2942186919"
   }
 });
